c971a2a466136ab565683dba1602d923fc918aeb
thismarvin <bend_@outlook.com>
2020-07-10 18:45:45 -0500

    got rid of vertex length check

--- src/Relatus/Maths/SchemaShape2D.cs
+++ src/Relatus/Maths/SchemaShape2D.cs
@@ -10,9 +10,6 @@ namespace Relatus.Maths
 
         public SchemaShape2D(Vector2[] vertices, LineSegment[] edges)
         {
-            if (vertices.Length != edges.Length)
-                throw new RelatusException("A 2D shape schema should have the same amount of vertices as edges.", new ArgumentException());
-
             Vertices = vertices;
             Edges = edges;
         }
