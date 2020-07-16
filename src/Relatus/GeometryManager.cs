0f29db0cc2b93a2cefa3ba872266b9dcdd9d815d
thismarvin <bend_@outlook.com>
2020-07-15 23:04:33 -0500

    made it so clockwise vertices are culled

--- src/Relatus/GeometryManager.cs
+++ src/Relatus/GeometryManager.cs
@@ -310,14 +310,14 @@ namespace Relatus
                 new Vector3[]
                 {
                     new Vector3(0, 0, 0),
+                    new Vector3(0, -1, 0),
+                    new Vector3(1, -1, 0),
                     new Vector3(1, 0, 0),
-                    new Vector3(1, 1, 0),
-                    new Vector3(0, 1, 0),
                 },
                 new short[]
                 {
-                    0, 1, 2,
-                    0, 2, 3
+                    0, 1, 3,
+                    3, 1, 2
                 },
                 true
             );
