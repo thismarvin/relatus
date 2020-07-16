919ba4bd9014594801fd7e63b6d79331635732ec
thismarvin <bend_@outlook.com>
2020-07-15 23:03:12 -0500

    sprites reflects camera changes

--- src/Relatus/Graphics/SpriteGroup.cs
+++ src/Relatus/Graphics/SpriteGroup.cs
@@ -32,7 +32,7 @@ namespace Relatus.Graphics
 
         public override void Draw(Camera camera)
         {
-            spriteBatch.Begin(SpriteSortMode.Deferred, sharedBlendState, sharedSamplerState, null, null, sharedEffect, camera.Transform);
+            spriteBatch.Begin(SpriteSortMode.Deferred, sharedBlendState, sharedSamplerState, null, null, sharedEffect, camera.SpriteTransform);
             {
                 for (int i = 0; i < groupIndex; i++)
                 {
