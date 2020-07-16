c7bd00cb61a7150352a6f7ab544c2fa0ed167128
thismarvin <bend_@outlook.com>
2020-07-15 23:01:37 -0500

    upgraded the camera some more!

--- src/Relatus/Camera.cs
+++ src/Relatus/Camera.cs
@@ -19,8 +19,10 @@ namespace Relatus
     {
         public string Name { get; private set; }
         public Matrix WVP { get; private set; }
+        public Matrix SpriteTransform { get; private set; }
 
         public RectangleF Bounds => new RectangleF(position.X, position.Y, width, height);
+        public Vector2 Center => new Vector2(position.X + width / 2, position.Y + height / 2);
 
         public Vector3 Position
         {
@@ -58,7 +60,6 @@ namespace Relatus
             }
         }
 
-        //public Matrix Transform { get; private set; }
         //public float Zoom { get; private set; }
         //public float Rotation { get; private set; }
 
@@ -90,6 +91,7 @@ namespace Relatus
             this.projectionType = projectionType;
 
             world = Matrix.Identity;
+            view = Matrix.CreateLookAt(position, target, up);
 
             switch (this.projectionType)
             {
@@ -102,6 +104,14 @@ namespace Relatus
             }
 
             WVP = world * view * projection;
+            SpriteTransform = CreateTransform();
+
+            WindowManager.WindowChanged += HandleWindowResize;
+        }
+
+        internal void HandleWindowResize(object sender, EventArgs e)
+        {
+            SpriteTransform = CreateTransform();
         }
 
         public static Camera CreateOrthographic(string name, float width, float height, float near, float far)
@@ -134,16 +144,23 @@ namespace Relatus
 
         public Camera SetPosition(float x, float y, float z)
         {
+            if (position.X == x && position.Y == y && position.Z == z)
+                return this;
+
             position = new Vector3(x, y, z);
 
             view = Matrix.CreateLookAt(position, target, up);
             WVP = world * view * projection;
+            SpriteTransform = CreateTransform();
 
             return this;
         }
 
         public Camera SetTarget(float x, float y, float z)
         {
+            if (target.X == x && target.Y == y && target.Z == z)
+                return this;
+
             target = new Vector3(x, y, z);
 
             view = Matrix.CreateLookAt(position, target, up);
@@ -154,6 +171,9 @@ namespace Relatus
 
         public Camera SetUp(float x, float y, float z)
         {
+            if (up.X == x && up.Y == y && up.Z == z)
+                return this;
+
             up = new Vector3(x, y, z);
 
             view = Matrix.CreateLookAt(position, target, up);
@@ -164,6 +184,9 @@ namespace Relatus
 
         public Camera SetWorld(Matrix world)
         {
+            if (this.world == world)
+                return this;
+
             this.world = world;
 
             WVP = world * view * projection;
@@ -173,6 +196,9 @@ namespace Relatus
 
         public Camera SetBounds(float width, float height)
         {
+            if (this.width == width && this.height == height)
+                return this;
+
             this.width = width;
             this.height = height;
 
@@ -191,86 +217,11 @@ namespace Relatus
             return this;
         }
 
-        //private void Initialize()
-        //{
-        //    UpdatePositions();
-        //    UpdateMatrices();
-        //}
-
-        //private void UpdatePositions()
-        //{
-        //    Zoom = WindowManager.Scale;
-
-        //    if (WindowManager.WideScreenSupported)
-        //    {
-        //        Bounds = new RectangleF
-        //        (
-        //            (int)TopLeft.X - WindowManager.PillarBox,
-        //            (int)TopLeft.Y - WindowManager.LetterBox,
-        //            WindowManager.PixelWidth + (int)Math.Ceiling(WindowManager.PillarBox * 2),
-        //            WindowManager.PixelHeight + (int)Math.Ceiling(WindowManager.LetterBox * 2)
-        //        );
-        //        cameraCenter = new Vector3(Bounds.Width / 2 - WindowManager.PillarBox, Bounds.Height / 2 - WindowManager.LetterBox, 0);
-        //    }
-        //    else
-        //    {
-        //        Bounds = new RectangleF
-        //        (
-        //            (int)TopLeft.X,
-        //            (int)TopLeft.Y,
-        //            WindowManager.PixelWidth,
-        //            WindowManager.PixelHeight
-        //        );
-        //        cameraCenter = new Vector3(Bounds.Width / 2, Bounds.Height / 2, 0);
-        //    }
-
-        //    position = new Vector3(Center.X, -Center.Y, 1);
-        //    target = new Vector3(position.X, position.Y, 0);
-        //}
-
-        //private void UpdateMatrices()
-        //{
-        //    Transform =
-        //        // M = R * T * S
-        //        // Translate the transform matrix to the inverse of the camera's center.
-        //        Matrix.CreateTranslation(-cameraCenter) *
-        //        // Rotate the camera relative to the center of the camera bounds.
-        //        Matrix.CreateRotationZ(Rotation) *
-        //        // Translate the transform matrix to the transform matrix to the inverse of the camera's top left.
-        //        Matrix.CreateTranslation(-TopLeft) *
-
-        //        // Scale the transform matrix by the camera's zoom.
-        //        Matrix.CreateScale(Zoom + zoomOffset) *
-
-        //        // Anchor the transform matrix to the center of the screen instead of the top left.
-        //        Matrix.CreateTranslation(new Vector3(WindowManager.WindowWidth / 2, WindowManager.WindowHeight / 2, 0)) *
-        //        Matrix.Identity;
-
-        //    world =
-        //        // To be honest, I am not sure why this works.
-        //        // Essentially it rotates the World matrix exactly how the Transform matrix works.
-        //        Matrix.CreateTranslation(-cameraCenter) *
-        //        Matrix.CreateRotationZ(Rotation) *
-        //        Matrix.CreateTranslation(cameraCenter) *
-
-        //        // In a right-handed coordinate system, the y-axis points upwards. By default, positioning a polygon follows this logic.
-        //        // However, when drawing a sprite using SpriteBatch the y-axis points downwards.
-        //        // Since SpriteBatch is predominately used, the World matrix is rotated so the y-axis also points downwards.
-        //        Matrix.CreateRotationX(MathHelper.Pi) *
-        //        Matrix.Identity;
-
-        //    view =
-        //        Matrix.CreateLookAt(position, target, Vector3.Up);
-
-        //    projection =
-        //        Matrix.CreateOrthographic(WindowManager.WindowWidth / (Zoom + zoomOffset), WindowManager.WindowHeight / (Zoom + zoomOffset), 0, 64);
-
-        //    WVP = world * view * projection;
-        //}
-
-        public virtual void Update()
+        private Matrix CreateTransform()
         {
-
+            return
+                Matrix.CreateTranslation(WindowManager.PillarBox - position.X, WindowManager.LetterBox - position.Y, 0) *
+                Matrix.CreateScale(WindowManager.Scale);
         }
     }
 }
