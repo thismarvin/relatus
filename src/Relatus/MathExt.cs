82dac9c417864a1a9ee4991a9efe8e00bb67318f
thismarvin <bend_@outlook.com>
2020-07-18 18:29:14 -0500

    added constrain methods

--- src/Relatus/MathExt.cs
+++ src/Relatus/MathExt.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,6 +19,31 @@ namespace Relatus
         public static double RemapRange(double current, double currentMin, double currentMax, double newMin, double newMax)
         {
             return newMin + (newMax - newMin) * (current - currentMin) / (currentMax - currentMin);
+        }
+
+        public static Vector2 ConstrainPoint(float x, float y, RectangleF boundary)
+        {
+            float xConstrained = x;
+            float yConstrained = y;
+
+            xConstrained = Math.Max(boundary.Left, xConstrained);
+            xConstrained = Math.Min(boundary.Right, xConstrained);
+
+            yConstrained = Math.Max(boundary.Bottom, yConstrained);
+            yConstrained = Math.Min(boundary.Top, yConstrained);
+
+            return new Vector2(xConstrained, yConstrained);
+        }
+
+        public static RectangleF ConstrainRectangle(RectangleF rectangle, RectangleF boundary)
+        {
+            Vector2 constrainedTopLeft = ConstrainPoint(rectangle.Left, rectangle.Top, boundary);
+            Vector2 constrainedBottomRight = ConstrainPoint(rectangle.Right, rectangle.Bottom, boundary);
+
+            float constrainedWidth = constrainedBottomRight.X - constrainedTopLeft.X;
+            float constrainedHeight = constrainedTopLeft.Y - constrainedBottomRight.Y;
+
+            return new RectangleF(constrainedTopLeft.X, constrainedTopLeft.Y, constrainedWidth, constrainedHeight);
         }
     }
 }
