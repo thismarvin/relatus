37226d4755c5859e8cd8094ada27ac1763b125ee
thismarvin <bend_@outlook.com>
2020-07-22 09:53:42 -0500

    QOL changes + 110% parity with old sprite

--- src/Relatus/Graphics/BetterSprite.cs
+++ src/Relatus/Graphics/BetterSprite.cs
@@ -8,20 +8,72 @@ namespace Relatus.Graphics
 {
     public class BetterSprite
     {
-        public float X { get; set; }
-        public float Y { get; set; }
-        public float Z { get; set; }
-        public float Rotation { get; set; }
-        public Vector2 RotationOffset { get; set; }
-        public Color Tint { get; set; }
-        public RectangleF SampleRegion { get; set; }
-
+        public Texture2D Texture
+        {
+            get => texture;
+            set => SetTexture(value);
+        }
+        public float X
+        {
+            get => x;
+            set => SetPosition(value, y, z);
+        }
+        public float Y
+        {
+            get => y;
+            set => SetPosition(x, value, z);
+        }
+        public float Z
+        {
+            get => z;
+            set => SetPosition(x, y, value);
+        }
+        public Vector3 Translation
+        {
+            get => translation;
+            set => SetTranslation(value.X, value.Y, value.Z);
+        }
+        public Vector3 Scale
+        {
+            get => scale;
+            set => SetScale(value.X, value.Y, value.Z);
+        }
+        public Vector2 RotationOffset
+        {
+            get => rotationOffset;
+            set => SetRotationOffset(value.X, value.Y);
+        }
+        public float Rotation
+        {
+            get => rotation;
+            set => SetRotation(value);
+        }
+        public Color Tint
+        {
+            get => tint;
+            set => SetTint(value);
+        }
+        public RectangleF SampleRegion
+        {
+            get => sampleRegion;
+            set => SetSampleRegion((int)value.X, (int)value.Y, (int)value.Width, (int)value.Height);
+        }
         public int Width { get; private set; }
         public int Height { get; private set; }
 
-        private readonly Texture2D texture;
-        private readonly float texelWidth;
-        private readonly float texelHeight;
+        private Texture2D texture;
+        private float texelWidth;
+        private float texelHeight;
+
+        private float x;
+        private float y;
+        private float z;
+        private Vector3 translation;
+        private Vector3 scale;
+        private Vector2 rotationOffset;
+        private float rotation;
+        private Color tint;
+        private RectangleF sampleRegion;
 
         private bool modelChanged;
         private bool textureChanged;
@@ -40,39 +92,96 @@ namespace Relatus.Graphics
             spriteShader = AssetManager.GetEffect("Relatus_SpriteShader");
         }
 
-        public BetterSprite(Texture2D texture)
+        public static BetterSprite Create()
         {
-            this.texture = texture;
-            texelWidth = 1f / texture.Width;
-            texelHeight = 1f / texture.Height;
+            return new BetterSprite();
+        }
 
-            SampleRegion = new RectangleF(110, 0, 70, 113);
+        public BetterSprite()
+        {
             Tint = Color.White;
-
-            Width = texture.Width;
-            Height = texture.Height;
+            Scale = Vector3.One;
 
             modelChanged = true;
             textureChanged = true;
+        }
 
-            ApplyChanges();
+        public BetterSprite SetTexture(Texture2D texture)
+        {
+            this.texture = texture;
+
+            texelWidth = 1f / texture.Width;
+            texelHeight = 1f / texture.Height;
+
+            textureChanged = true;
+
+            return this;
         }
 
         public BetterSprite SetPosition(float x, float y, float z)
         {
-            X = x;
-            Y = y;
-            Z = z;
+            this.x = x;
+            this.y = y;
+            this.z = z;
 
             modelChanged = true;
 
             return this;
         }
 
+        public BetterSprite SetTranslation(float x, float y, float z)
+        {
+            translation = new Vector3(x, y, z);
+
+            modelChanged = true;
+
+            return this;
+        }
+
+        public BetterSprite SetScale(float x, float y, float z)
+        {
+            scale = new Vector3(x, y, z);
+
+            modelChanged = true;
+
+            return this;
+        }
+
+        public BetterSprite SetRotationOffset(float x, float y)
+        {
+            rotationOffset = new Vector2(x, y);
+
+            modelChanged = true;
+
+            return this;
+        }
+
+        public BetterSprite SetRotation(float rotation)
+        {
+            this.rotation = rotation;
+
+            modelChanged = true;
+
+            return this;
+        }
+
+        public BetterSprite SetTint(Color tint)
+        {
+            this.tint = tint;
+
+            textureChanged = true;
+
+            return this;
+        }
+
         public BetterSprite SetSampleRegion(int x, int y, int width, int height)
         {
-            SampleRegion = new RectangleF(x, y, width, height);
+            sampleRegion = new RectangleF(x, y, width, height);
 
+            Width = width;
+            Height = height;
+
+            textureChanged = true;
             modelChanged = true;
 
             return this;
@@ -80,13 +189,13 @@ namespace Relatus.Graphics
 
         public BetterSprite ApplyChanges()
         {
-            Vector2 topLeft = new Vector2((float)MathExt.RemapRange(SampleRegion.X, 0, texture.Width, 0, 1), (float)MathExt.RemapRange(SampleRegion.Y, 0, texture.Height, 0, 1));
-            Vector2 topRight = topLeft + new Vector2(texelWidth * SampleRegion.Width, 0);
-            Vector2 bottomRight = topLeft + new Vector2(texelWidth * SampleRegion.Width, texelHeight * SampleRegion.Height);
-            Vector2 bottomLeft = topLeft + new Vector2(0, texelHeight * SampleRegion.Height);
-
             if (textureChanged)
             {
+                Vector2 topLeft = new Vector2((float)MathExt.RemapRange(SampleRegion.X, 0, texture.Width, 0, 1), (float)MathExt.RemapRange(SampleRegion.Y, 0, texture.Height, 0, 1));
+                Vector2 topRight = topLeft + new Vector2(texelWidth * SampleRegion.Width, 0);
+                Vector2 bottomRight = topLeft + new Vector2(texelWidth * SampleRegion.Width, texelHeight * SampleRegion.Height);
+                Vector2 bottomLeft = topLeft + new Vector2(0, texelHeight * SampleRegion.Height);
+
                 textureBuffer?.Dispose();
                 textureBuffer = new DynamicVertexBuffer(graphicsDevice, typeof(VertexColorTexture), 4, BufferUsage.WriteOnly);
                 textureBuffer.SetData(new VertexColorTexture[]
@@ -120,8 +229,8 @@ namespace Relatus.Graphics
 
         internal VertexTransform GetVertexTransformColor()
         {
-            Vector3 scale = new Vector3(SampleRegion.Width, SampleRegion.Height, 1);
-            Vector3 translation = new Vector3(X, Y, Z);
+            Vector3 scale = new Vector3(SampleRegion.Width * Scale.X, SampleRegion.Height * Scale.Y, Scale.Z);
+            Vector3 translation = new Vector3(X + Translation.X, Y + Translation.Y, Z + Translation.Z);
 
             return new VertexTransform(scale, RotationOffset, Rotation, translation);
         }
