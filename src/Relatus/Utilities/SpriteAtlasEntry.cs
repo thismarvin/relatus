38cc6dd3276059e7cd93c74e0e6f9be1a7c4e016
thismarvin <bend_@outlook.com>
2020-07-29 22:24:10 -0500

    atlas entry makes use of new image region struct

--- src/Relatus/Utilities/SpriteAtlasEntry.cs
+++ src/Relatus/Utilities/SpriteAtlasEntry.cs
@@ -7,20 +7,14 @@ namespace Relatus.Utilities
     public class SpriteAtlasEntry
     {
         public string Name { get; private set; }
-        public int X { get; private set; }
-        public int Y { get; private set; }
-        public int Width { get; private set; }
-        public int Height { get; private set; }
+        public ImageRegion ImageRegion { get; private set; }
         public int OffsetX { get; private set; }
         public int OffsetY { get; private set; }
 
         internal SpriteAtlasEntry(string name, int x, int y, int width, int height, int offsetX, int offsetY)
         {
             Name = name;
-            X = x;
-            Y = y;
-            Width = width;
-            Height = height;
+            ImageRegion = new ImageRegion(x, y, width, height);
             OffsetX = offsetX;
             OffsetY = offsetY;
         }
