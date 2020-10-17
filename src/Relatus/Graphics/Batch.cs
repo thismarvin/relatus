f8bf30036aed96b8d8c1cf6f5352296a9a7affe8
thismarvin <bend_@outlook.com>
2020-10-16 22:41:25 -0500

    even though there wasnt a memory leak, i am pretty sure i need to dispose of the buffers...

--- src/Relatus/Graphics/Batch.cs
+++ src/Relatus/Graphics/Batch.cs
@@ -1,17 +1,20 @@
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Relatus.Graphics
 {
-    public class Batch
+    public class Batch : IDisposable
     {
         public VertexBufferBinding[] VertexBufferBindings { get; set; }
         public IndexBuffer IndexBuffer { get; set; }
+        public IDisposable[] Disposables { get; set; }
         public BatchExecution BatchExecution { get; set; }
         public PrimitiveType PrimitiveType { get; set; }
         public int TotalPrimitives { get; set; }
         public int Instances { get; set; }
 
         private static readonly GraphicsDevice graphicsDevice;
+
         static Batch()
         {
             graphicsDevice = Engine.Graphics.GraphicsDevice;
@@ -31,7 +34,23 @@ namespace Relatus.Graphics
                     graphicsDevice.DrawInstancedPrimitives(PrimitiveType, 0, 0, TotalPrimitives, Instances);
                     break;
             }
-
         }
+
+        #region IDisposable Support
+        private bool disposedValue;
+
+        public void Dispose()
+        {
+            if (!disposedValue)
+            {
+                for (int i = 0; i < Disposables.Length; i++)
+                {
+                    Disposables[i].Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+        #endregion
     }
 }
