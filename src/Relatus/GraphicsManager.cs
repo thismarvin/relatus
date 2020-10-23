4d81f85d3b6ff25c05e8443a1d467016c26a197c
thismarvin <bend_@outlook.com>
2020-10-22 22:36:43 -0500

    added a static readonly reference to the primary graphics device

--- src/Relatus/GraphicsManager.cs
+++ src/Relatus/GraphicsManager.cs
@@ -9,23 +9,18 @@ namespace Relatus
     public static class GraphicsManager
     {
         public static RasterizerState DefaultRasterizerState { get; private set; }
-        public static RasterizerState ScissorRasterizerState { get; private set; }
         public static RasterizerState DebugRasterizerState { get; private set; }
-
         public static Texture2D SimpleTexture { get; private set; }
         public static Texture2D SimpleNormalTexture { get; private set; }
 
-        public static RasterizerState RasterizerState => DebugManager.ShowWireFrame ? DebugRasterizerState : DefaultRasterizerState;
+        private static readonly GraphicsDevice graphicsDevice;
 
         static GraphicsManager()
         {
+            graphicsDevice = Engine.Graphics.GraphicsDevice;
+
             DefaultRasterizerState = new RasterizerState()
             {
-                CullMode = CullMode.CullClockwiseFace
-            };
-            ScissorRasterizerState = new RasterizerState()
-            {
-                ScissorTestEnable = true,
                 CullMode = CullMode.CullClockwiseFace
             };
             DebugRasterizerState = new RasterizerState
@@ -34,17 +29,16 @@ namespace Relatus
                 CullMode = CullMode.None
             };
 
-            SimpleTexture = new Texture2D(Engine.Graphics.GraphicsDevice, 1, 1);
+            SimpleTexture = new Texture2D(graphicsDevice, 1, 1);
             SimpleTexture.SetData(new[] { Color.White });
 
-            SimpleNormalTexture = new Texture2D(Engine.Graphics.GraphicsDevice, 1, 1);
+            SimpleNormalTexture = new Texture2D(graphicsDevice, 1, 1);
             SimpleNormalTexture.SetData(new[] { ColorExt.CreateFromHex(0x7f7fff) });
         }
 
         internal static void UnloadContent()
         {
             DefaultRasterizerState.Dispose();
-            ScissorRasterizerState.Dispose();
             DebugRasterizerState.Dispose();
 
             SimpleTexture.Dispose();
